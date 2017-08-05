c186266a5a7efdb1b91059b5fd83fcc2ce84cd29
Jackie <jacklinejap@gmail.com>
2017-08-05 15:00:15 +1000

    Updated settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hbusbg=8eer@7dh&^4vc^uujg3x!!o1rcbaphqynr8=%-7x^&o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'jackline55.pythonanywhere.com']
 
 
 # Application definition
